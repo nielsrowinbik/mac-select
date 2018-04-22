4659f793f9e82030dde24e6f5fc370c63caf447a
Niels Bik <nielsrowinbik@gmail.com>
2018-04-22 15:02:14 +0200

    Make Navigation responsive

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,17 +2,68 @@ import React from 'react';
 import styled from 'styled-components';
 import { container } from '../mixins';
 
-const UnstyledNavigation = ({ children, ...props }) => <header {...props}><div>{ children }</div></header>;
+const UnstyledNavButton = ({ isOpen, ...props }) => <div {...props}><div /></div>;
+const NavButton = styled(UnstyledNavButton)`
+	align-items: center;
+	cursor: pointer;
+	display: flex;
+	height: 20px;
+	width: 25px;
+
+	& > div {
+		background-color: ${({ isOpen }) => isOpen ? 'transparent' : 'currentColor'};
+		cursor: pointer;
+		height: 2px;
+		position: relative;
+		width: 100%;
+
+		&:before,
+		&:after {
+			background-color: currentColor;
+			content: "";
+			height: 2px;
+			position: absolute;
+			width: 100%;
+		}
+
+		&:before {
+			top: ${({ isOpen }) => isOpen ? 0 : -8}px;
+			transform: rotate(${({ isOpen }) => isOpen ? -45 : 0}deg);
+		}
+
+		&:after {
+			top: ${({ isOpen }) => isOpen ? 0 : 8}px;
+			transform: rotate(${({ isOpen }) => isOpen ? 45 : 0}deg);
+		}
+	}
+
+	@media (min-width: 701px) {
+		display: none;
+	}
+`;
+
+const UnstyledNavigation = ({ children, isOpen, toggleOpen, ...props }) => (
+	<header {...props}>
+		<div>
+			{ children }
+			<NavButton
+				isOpen={isOpen}
+				onClick={toggleOpen}
+			/>
+		</div>
+	</header>
+);
 const Navigation = styled(UnstyledNavigation)`
-	background-color: #ffffff;
-	box-shadow: inset 0 -1px 0 0 rgba(0, 0, 0, .05);
-	
+	background-color: #fff;
+	box-shadow: inset 0 -1px rgba(0, 0, 0, 0.05);
+	height: 76px;
+
 	& > div {
 		${container}
 		align-items: center;
 		display: flex;
-		padding: 15px 0;
-		height: 76px;
+		height: 100%;
+		justify-content: space-between;
 
 		h1,
 		a {
@@ -22,36 +73,66 @@ const Navigation = styled(UnstyledNavigation)`
 		}
 
 		h1 {
-			margin: 0;
-			flex: auto;
 			font-size: 1.85rem;
+			margin: 0;
 		}
 
-		:not(h1) > a {
-			padding: 5px 10px;
-			margin: 0 5px;
-			position: relative;
-			font-weight: 400;
+		nav {
+			background-color: #ffffff;
+			display: ${({ isOpen }) => isOpen ? 'flex' : 'none'};
+			flex-direction: column;
+			left: 0;
+			position: absolute;
+			top: 76px;
+			width: 100%;
 
-			:hover {
-				color: ${({ theme }) => theme.text.secondary};
+			a {
+				box-shadow: inset 0 -1px 0 0 rgba(0,0,0,0.05);
+				font-size: 1rem;
+				font-weight: 400;
+				text-align: center;
+				padding: 20px 0;
+				position: relative;
+
+				&:hover {
+					color: #8e8e8e;
+				}
 			}
 
-			:not(:last-child) {
-				margin-right: 10px;
-			}
+			@media (min-width: 701px) {
+				display: flex;
+				flex-direction: row;
+				flex-wrap: nowrap;
+				left: auto;
+				position: relative;
+				top: auto;
+				width: auto;
+				
+				a {
+					padding: 5px 10px;
+					box-shadow: none;
 
-			:last-child {
-				padding-left: 20px;
+					&:not(:first-child) {
+						margin-left: 10px;
+					}
 
-				:after {
-					content: '';
-					position: absolute;
-					left: 0;
-					top: 0;
-					bottom: 0;
-					width: 1px;
-					background-color: ${({ theme }) => theme.text.primary};
+					&:not(:last-child) {
+						margin-right: 10px;
+					}
+
+					&:last-child {
+						padding-left: 20px;
+
+						&:before {
+							content: '';
+							position: absolute;
+							top: 0;
+							left: 0;
+							height: 100%;
+							width: 1px;
+							background-color: #444;
+						}
+					}
 				}
 			}
 		}
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,5 +1,5 @@
 import 'typeface-open-sans';
-import React from 'react';
+import React, { Component } from 'react';
 import Helmet from 'react-helmet';
 import Link from 'gatsby-link';
 import Footer from '../components/Footer';
@@ -9,49 +9,62 @@ import { global, theme } from '../mixins';
 
 injectGlobal`${global}`;
 
-const TemplateWrapper = ({ children }) => (
-	<ThemeProvider {...theme}>
-		<React.Fragment>
-			<Helmet
-				title="Mac Select - Het adres voor het kopen en verkopen van een tweedehands Mac, iMac, of Macbook"
-				titleTemplate="%s - Mac Select"
-			/>
-			<Navigation>
-				<h1><Link exact to="/">Mac Select</Link></h1>
-				<Link activeStyle={{}} to="/aanbod">Aanbod</Link>
-				<Link activeStyle={{}} to="/verkopen">Verkopen</Link>
-				<Link activeStyle={{}} to="/hulp">Hulp</Link>
-				<Link activeStyle={{}} to="/contact">Contact</Link>
-				<Link activeStyle={{}} to="/blog">Blog</Link>
-			</Navigation>
-			{ children() }
-			<Footer>
-				<section>
-					<div>
-						<h4>Mac Select</h4>
-						<Link to="/">Home</Link>
-						<Link to="/aanbod">Huidig aanbod</Link>
-						<Link to="/verkopen">Uw Mac verkopen</Link>
-					</div>
-					<div>
-						<h4>Hulp en informatie</h4>
-						<Link to="/blog">Mac Select blog</Link>
-						<Link to="/hulp">Hulp bij aan- en verkoop</Link>
-						<Link to="/hulp#faq">Veelgestelde vragen</Link>
-					</div>
-					<div>
-						<h4>Contact</h4>
-						<a href="tel:+31647028625">Telefoon</a>
-						<a href="mailto:info@mac-select.nl">Email</a>
-						<a href="https://www.facebook.com/macselect">Facebook</a>
-					</div>
-				</section>
-				<section>
-					<span>Copyright © 2018 Mac Select. Website gerealiseerd door <a href="https://nielsbik.nl">Niels Bik</a>.</span>
-				</section>
-			</Footer>
-		</React.Fragment>
-	</ThemeProvider>
-);
+class TemplateWrapper extends Component {
+	state = {
+		isOpen: false,
+		toggleOpen: () => this.setState({ isOpen: !this.state.isOpen })
+	}
+
+	render = () => {
+		const { children } = this.props;
+
+		return (
+			<ThemeProvider {...theme}>
+				<React.Fragment>
+					<Helmet
+						title="Mac Select - Het adres voor het kopen en verkopen van een tweedehands Mac, iMac, of Macbook"
+						titleTemplate="%s - Mac Select"
+					/>
+					<Navigation {...this.state}>
+						<Link exact to="/"><h1>Mac Select</h1></Link>
+						<nav onClick={this.state.toggleOpen}>
+							<Link activeStyle={{}} to="/aanbod">Aanbod</Link>
+							<Link activeStyle={{}} to="/verkopen">Verkopen</Link>
+							<Link activeStyle={{}} to="/hulp">Hulp</Link>
+							<Link activeStyle={{}} to="/contact">Contact</Link>
+							<Link activeStyle={{}} to="/blog">Blog</Link>
+						</nav>
+					</Navigation>
+					{ children() }
+					<Footer>
+						<section>
+							<div>
+								<h4>Mac Select</h4>
+								<Link to="/">Home</Link>
+								<Link to="/aanbod">Huidig aanbod</Link>
+								<Link to="/verkopen">Uw Mac verkopen</Link>
+							</div>
+							<div>
+								<h4>Hulp en informatie</h4>
+								<Link to="/blog">Mac Select blog</Link>
+								<Link to="/hulp">Hulp bij aan- en verkoop</Link>
+								<Link to="/hulp#faq">Veelgestelde vragen</Link>
+							</div>
+							<div>
+								<h4>Contact</h4>
+								<a href="tel:+31647028625">Telefoon</a>
+								<a href="mailto:info@mac-select.nl">Email</a>
+								<a href="https://www.facebook.com/macselect">Facebook</a>
+							</div>
+						</section>
+						<section>
+							<span>Copyright © 2018 Mac Select. Website gerealiseerd door <a href="https://nielsbik.nl">Niels Bik</a>.</span>
+						</section>
+					</Footer>
+				</React.Fragment>
+			</ThemeProvider>
+		);
+	}
+}
 
 export default TemplateWrapper;
