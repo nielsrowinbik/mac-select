9f5054a52fcafcae59081055a0a2b06cf348066c
Niels Bik <nielsrowinbik@gmail.com>
2018-04-23 20:53:14 +0200

    Fix query and add Spotlight

---
+++ src/components/Spotlight.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import styled from 'styled-components';
+import Link from 'gatsby-link';
+import { container } from '../mixins';
+import { get } from 'lodash';
+
+const Header = styled.div`
+	text-align: center;
+`;
+
+const Hr = styled.div`
+    background-color: #cecece;
+	border: none;
+    height: 1px;
+    margin: 40px auto 60px;
+	position: relative;
+    width: 95%;
+
+	:before {
+		background-color: #fff;
+		border-bottom-left-radius: 28px;
+		border-bottom-right-radius: 28px;
+		box-shadow: 2px 3px 12px -3px rgba(0,0,0,.4);
+		content: "";
+		height: 16px;
+		left: 50%;
+		padding-top: 4px;
+		position: absolute;
+		top: 1px;
+		transform: translateX(-50%);
+		width: 28px;
+	}
+
+	:after {
+		content: "v";
+		left: 50%;
+		position: absolute;
+		top: -3px;
+		transform: translateX(-50%) scale(1.8,.8);
+	}
+`;
+
+const ImageContainer = styled(Link)`
+	display: block;
+	margin: 0 auto;
+	width: 75%;
+
+	& > img {
+		width: 100%;
+	}
+
+	@media (min-width: 701px) {
+		width: 52%;
+	}
+`;
+
+const UnstyledSpotlight = ({ product, ...props }) => (
+	<section {...props}>
+		<Header>
+			<h1>Uw beste optie</h1>
+			<p>De beste tweedehands {product.type} die Mac Select u op dit moment te bieden heeft.</p>
+			<p><Link to={`/aandbod/${product.fields.slug}`}>Meer informatie</Link></p>
+		</Header>
+		<Hr />
+		<ImageContainer to={`/aanbod/${product.fields.slug}`}>
+			<img src={get(product, 'images[0].image')} />
+		</ImageContainer>
+	</section>
+);
+
+const Spotlight = styled(UnstyledSpotlight)`
+	${container}
+`;
+
+export { Spotlight };
+export default Spotlight;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,20 @@
-import { filter, get, map, take } from 'lodash';
+import { filter, find, get, map, take } from 'lodash';
 import React from 'react';
 import Main from '../components/Main';
 import Hero from '../components/Hero';
 import Link from 'gatsby-link';
 import SleekGrid, { SleekGridItem } from '../components/SleekGrid';
-import { isBlogPost, isProduct } from '../helpers';
+import { isBlogPost, isProduct, isSpotlight } from '../helpers';
+import Spotlight from '../components/Spotlight';
 
 const Home = (props) => {
-	const { data = {} } = props;
-	const { allFile = {} } = data;
+	const { data } = props;
+	const { allFile } = data;
 	const { edges } = allFile;
 
 	const posts = filter(edges, isBlogPost);
 	const products = filter(edges, isProduct);
+	const spotlight = get(find(products, isSpotlight), 'node.childAanbodJson');
 
 	return (
 		<Main bg="#f6f6f6">
@@ -24,27 +26,28 @@ const Home = (props) => {
 					Mac Select heeft uitsluitend geselecteerde en betrouwbare tweedehands Macs te koop.
 				</p>
 			</Hero>
+			<Spotlight product={spotlight} />
 			<Hero src="/assets/images/mac_book_pro_2016_alt.png" rtl>
 				<h1>Uw Mac verkopen</h1>
-				<h2><Link to="/verkopen">Doe de prijscheck</Link></h2>
+				<h2><Link to="/contact">Neem contact op</Link></h2>
 				<p>
-					Bent u van plan uw Mac te verkopen? Check binnen één minuut wat u van Mac Select voor uw Mac kunt krijgen en maak
-					met één druk op de knop de verkoop rond.
+					Bent u van plan uw Mac te verkopen? Neem contact op met Mac Select en u weet direct wat u kunt krijgen voor uw
+					tweedehands Mac. Meer informatie over het verkopen van uw Mac aan Mac Select vindt u <Link to="/hulp#verkoop">hier</Link>.
 				</p>
 			</Hero>
 			<SleekGrid>
-				{ products.length >= 2 && map(take(products, 2), ({ node: { childMarkdownRemark: product } }) => (
+				{ products.length >= 2 && map(take(products, 2), ({ node: { childAanbodJson: product } }) => (
 					<SleekGridItem
 						cta={{ ctaValue: `/aanbod${get(product, 'fields.slug')}`, ctaText: 'Bekijk product' }}
 						isLarge
 						key={get(product, 'fields.slug')}
-						src={get(product, 'frontmatter.images[0].image')}
-						title={get(product, 'frontmatter.title')}
+						src={get(product, 'images[0].image')}
+						title={get(product, 'title')}
 					/>
 				)) }
 				{ posts.length >= 2 && map(take(posts, 2), ({ node: { childMarkdownRemark: post } }) => (
 					<SleekGridItem
-						cta={{ ctaValue: `/blog${get(post, 'fields.slug')}`, ctaText: 'Lees blog' }}
+						cta={{ ctaValue: `/blog/${get(post, 'fields.slug')}`, ctaText: 'Lees blog' }}
 						key={get(post, 'fields.slug')}
 						src={get(post, 'frontmatter.banner')}
 						title={get(post, 'frontmatter.title')}
@@ -58,27 +61,34 @@ const Home = (props) => {
 export default Home;
 
 // eslint-disable-next-line no-undef
-// export const pageQuery = graphql`
-// 	query IndexQuery {
-// 		allFile(filter: { name: { ne:".gitinclude" } }) {
-// 			edges {
-// 				node {
-// 					sourceInstanceName
-// 					childMarkdownRemark {
-// 						id
-// 						fields {
-// 							slug
-// 						}
-// 						frontmatter {
-// 							banner
-// 							title
-// 							images {
-// 								image
-// 							}
-// 						}
-// 					}
-// 				}
-// 			}
-// 		}
-// 	}
-// `;
+export const pageQuery = graphql`
+	query IndexQuery {
+		allFile(filter: { name: { ne:".gitinclude" } }) {
+			edges {
+				node {
+					sourceInstanceName
+					childAanbodJson {
+						fields {
+							slug
+						}
+						images {
+							image
+						}
+						spotlight
+						title
+						type
+					}
+					childMarkdownRemark {
+						fields {
+							slug
+						}
+						frontmatter {
+							banner
+							title
+						}
+					}
+				}
+			}
+		}
+	}
+`;
